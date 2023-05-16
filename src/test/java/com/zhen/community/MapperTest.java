c29ad8368087465e6e2f77902881c88d74892e6e
zhen <1574392897@qq.com>
2023-05-16 16:13:37 +0800

    注释

--- src/test/java/com/zhen/community/MapperTest.java
+++ src/test/java/com/zhen/community/MapperTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Date;
 import java.util.List;
 
+//aa
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration(classes = CommunityApplication.class)
